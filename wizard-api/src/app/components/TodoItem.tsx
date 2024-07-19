e27eb5a18d30dd7d01f9973daeb5aa96c2e30b4a
Alex Saines <46796104+adsaines@users.noreply.github.com>
2024-07-18 23:07:31 -0400

    went over the basics of react

--- wizard-api/src/app/components/TodoItem.tsx
+++ wizard-api/src/app/components/TodoItem.tsx
@@ -1,13 +1,75 @@
 // Lesson 1: Make a component that displays something!
+'use client'
 
-export const TodoItem = () => {
+import { useState } from "react";
+
+export interface TodoItemProps {
+    task: string;
+    completed?: boolean;
+    completeByTime?: Date;
+    dependentTasks?: string[];
+}
+
+export const TodoItem = ({
+    task,
+    completed = false,
+    completeByTime = new Date(),
+    dependentTasks = ['None']
+}: TodoItemProps) => {
+
+    const [myTask, setMyTask] = useState(task)
+
+    const roundAboutSetMyTask = (str: string) => {
+        console.log(`My new task value: ${str}`);
+        setMyTask(str);
+    }
+
+    const taskTernaryExample = task ? task : `Undefined`
+    let taskIfExample: string;
+
+    if(task) {
+        taskIfExample = task;
+    } else {
+        taskIfExample = 'Still Undefined'
+    }
+
+    const typescriptFun = task ?? 'value if task is undefined';
+
     return (
         <div className="flex">
-            <input type="checkbox" />
-            <span className="h-5">Buy Groceries</span>
-            <div className="flex">
-                <button onClick={() => alert(`I will make a copy of this item and place it at the bottom of the list.`)}>Copy</button>
-                <button onClick={() => alert(`I will remove this item from the app`)}>Delete</button>
+            <div className="pr-2">
+                <input type="checkbox" checked={completed} />
+            </div>
+            <div className="pr-2">
+                <input 
+                    value={myTask} 
+                    onChange={(event) => {
+                        console.log(event)
+                        roundAboutSetMyTask(event.target.value)
+                    }} 
+                />
+            </div>
+            <div className="pr-2">
+                <p className="h-5">task: {task}</p>
+                <p className="h-5">taskTernaryExample: {taskTernaryExample}</p>
+                <p className="h-5">taskIfExample: {taskIfExample}</p>
+                <p className="h-5">typescriptFun: {typescriptFun}</p>
+            </div>
+            <div className="pr-2">
+                {completeByTime.toLocaleDateString()}
+            </div>
+            <div className="pr-2">
+                <ul>
+                    {
+                        dependentTasks?.map((task, count) => {
+                            return (
+                                <li key={count}>
+                                    {task}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         </div>
     )
--- wizard-api/src/app/components/TodoList.tsx
+++ wizard-api/src/app/components/TodoList.tsx
@@ -1,15 +1,39 @@
 // Lesson 2: use that thing that you can display to create a dynamic display
+'use client'
 
-import { TodoItem } from "./TodoItem"
+import { TodoItem, TodoItemProps } from "./TodoItem"
 
 export const TodoList = () => {
+
+    const myTodoItems: TodoItemProps[] = [
+        {
+            task: 'Laundry',
+            completeByTime: new Date('2024/7/18 12:00')
+        },
+        {
+            task: 'Shower',
+            completeByTime: new Date('2024/7/18 05:00')
+        },
+        {
+            task: 'Cleanup room',
+            completeByTime: new Date('2024/7/20')
+        }
+    ]
+
     return (
         <ul>
-            <li>
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
-            </li>
+            {
+                myTodoItems.map((todo, count) => {
+                    return (
+                        <li key={count} className="pb-2">
+                            <TodoItem 
+                                task={todo.task}
+                                completeByTime={todo.completeByTime}
+                            />
+                        </li>
+                    )
+                })
+            }
         </ul>
     )
 }
