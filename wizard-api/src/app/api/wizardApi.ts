f162aefaab30dd26f9b545031635cea9acb40e06
Alex Saines <46796104+adsaines@users.noreply.github.com>
2024-08-01 21:07:39 -0400

    added next step doc in file

---
+++ wizard-api/src/app/api/wizardApi.ts
@@ -0,0 +1,22 @@
+/**
+ * TODO: Next step...
+ * 
+ * Read about "fetch" in JS: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ * 
+ * Implement fetch to retrieve the "Beginner Elixirs" from the free wizard API listed in the readme
+ * 
+ */
+
+
+const enum elixirDifficulty {
+    'Unknown'
+    , 'Advanced'
+    , 'Moderate'
+    , 'Beginner'
+    , 'OrdinaryWizardingLevel'
+    , 'OneOfAKind'
+}
+
+const fetchElixirList = (difficulty: elixirDifficulty) => {
+    return [];
+}
